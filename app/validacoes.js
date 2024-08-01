69a8191f24411d5aaffb5243469d6d18484b3813
Josiane <josibel016@yahoo.com.br>
2024-08-01 09:28:01 +0100

    end of the game

--- app/validacoes.js
+++ app/validacoes.js
@@ -1,14 +1,34 @@
 function verificaSeOChutePossuiValorValido(chute){
     const numero = +chute;
 
-    if (chuteforInvalido(numero)){
-        console.log('valor invalido')
+    
+
+    if (chuteforInvalido(numero)) {
+        elementoChute.innerHTML += `<div>valor inválido</div>`;
+        return
     }
 
     if (numeroForMaiorQueOPermitido(numero)){
-        console.log(`valor invalido: o numero precisa estar entre ${menorValor} e ${maiorValor}`)
+        elementoChute.innerHTML +=`<div>valor invalido: o numero precisa estar entre ${menorValor} e ${maiorValor}</div>`
+    return
     }
-}
+    if (numero === numeroSecreto){
+        document.body.innerHTML=`
+        <h2> Voce Acertou!!!</h2>
+        <h3> o numero secreto era ${numeroSecreto}</h3> 
+        <button id=jogar-novamente class="btn-jogar">jogar Novamente</button>
+        `
+        
+
+    } else if (numero < numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`;
+    } else{
+        elementoChute.innerHTML += `
+            <div>O número secreto é menos <i class="fa-solid fa-down-long"></i></div>`;
+
+    }
+    }
 
 function chuteforInvalido(numero) {
     return Number.isNaN(numero);
@@ -17,3 +37,10 @@ function chuteforInvalido(numero) {
 function numeroForMaiorQueOPermitido(numero){
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click',e =>{
+    if(e.target.id =='jogar Novamente'){
+        window.location.reload()
+    }
+
+})
