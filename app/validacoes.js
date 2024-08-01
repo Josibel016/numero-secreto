ce306b50205d2c11ef0fe8dd8335329982b3fdb0
Josiane <josibel016@yahoo.com.br>
2024-08-01 10:18:37 +0100

    some corretions

--- app/validacoes.js
+++ app/validacoes.js
@@ -1,46 +1,49 @@
-function verificaSeOChutePossuiValorValido(chute){
+function verificaSeOChutePossuiValorValido(chute) {
     const numero = +chute;
 
-    
-
     if (chuteforInvalido(numero)) {
-        elementoChute.innerHTML += `<div>valor inválido</div>`;
-        return
+        if (chute.toUpperCase() === 'GAME OVER') {
+            document.body.innerHTML = `
+                <h2> Game Over </h2>
+                <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+            `;
+            document.body.style.backgroundColor = "black";
+        } else {
+            elementoChute.innerHTML += `<div>valor inválido</div>`;
+            return;
+        }
     }
 
-    if (numeroForMaiorQueOPermitido(numero)){
-        elementoChute.innerHTML +=`<div>valor invalido: o numero precisa estar entre ${menorValor} e ${maiorValor}</div>`
-    return
+    if (numeroForMaiorQueOPermitido(numero)) {
+        elementoChute.innerHTML += `<div>valor inválido: o número precisa estar entre ${menorValor} e ${maiorValor}</div>`;
+        return;
     }
-    if (numero === numeroSecreto){
-        document.body.innerHTML=`
-        <h2> Voce Acertou!!!</h2>
-        <h3> o numero secreto era ${numeroSecreto}</h3> 
-        <button id=jogar-novamente class="btn-jogar">jogar Novamente</button>
-        `
-        
 
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2> Você Acertou!!!</h2>
+            <h3> O número secreto era ${numeroSecreto}</h3> 
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `;
     } else if (numero < numeroSecreto) {
         elementoChute.innerHTML += `
             <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`;
-    } else{
+    } else {
         elementoChute.innerHTML += `
-            <div>O número secreto é menos <i class="fa-solid fa-down-long"></i></div>`;
-
+            <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`;
     }
-    }
+}
 
 function chuteforInvalido(numero) {
     return Number.isNaN(numero);
 }
 
-function numeroForMaiorQueOPermitido(numero){
-    return numero > maiorValor || numero < menorValor
+function numeroForMaiorQueOPermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
 }
 
-document.body.addEventListener('click',e =>{
-    if(e.target.id =='jogar Novamente'){
-        window.location.reload()
+document.body.addEventListener('click', e => {
+    if (e.target.id === 'jogar-novamente') {
+        window.location.reload();
     }
-
-})
+});
